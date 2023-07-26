066518537feeec74149b4da56aa0cdea35afbd53
Sakthe-Balan <sakthebalan2003@gmail.com>
2023-07-26 15:14:26 +0530

    changes to the tamplate

--- main.cpp
+++ main.cpp
@@ -5,29 +5,22 @@
 using namespace std;
 class utils{
     public:
-    void help() {
-    std::cout << "Available commands:\n";
-    std::cout << "  help : Display available commands.\n";
-    // Add more command descriptions here
-    std::cout << "  exit : Exit the shell.\n";
-}
-    void execute(string input){
-        
-       
-    }
+
     
 void executeCommand(const string& input) {
-    // Implement your custom command execution here
+   
      if (input == "help") {
            help();
-        } else {
-            execute(input);
         }
-    // You can parse the command string and take appropriate actions based on the input.
+    }
+void help() {
+    std::cout << "Available commands:\n";
+    std::cout << "  help : Display available commands.\n";
+    std::cout << "  exit : Exit the shell.\n";
 }
 
 };
-// Function prototypes
+
 
 
 
